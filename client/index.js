5cf227b3a336d0059379e3b03253649f90d6250e
Raman Kazimirchyk <t2n3qqq@gmail.com>
2016-11-18 17:35:27 +0300

    Add tmthng

--- client/index.js
+++ client/index.js
@@ -6,76 +6,43 @@ fetch(`http://localhost:3005/file/ch1.bin`, {
   .then(data => {
     console.log('DATA', data);
     // Create a timer
-    var start = +new Date();
+    const xValues = data.values.map((item, index) => data.dataBlockReceiveTime/data.chanelPoints * (index + 1));
+    // const max = _.max(...data.values);
+    // const min = _.min(...data.values);
+    // console.log(min, max);
+    const start = +new Date();
 
     // Create the chart
-    Highcharts.stockChart('container', {
-        chart: {
-            events: {
-                load: function () {
-                    this.setTitle(null, {
-                        text: 'Built chart in ' + (new Date() - start) + 'ms'
-                    });
-                }
-            },
-            zoomType: 'x'
-        },
+    var orgHighchartsRangeSelectorPrototypeRender = Highcharts.RangeSelector.prototype.render;
+    Highcharts.RangeSelector.prototype.render = function (min, max) {
+        orgHighchartsRangeSelectorPrototypeRender.apply(this, [min, max]);
+        var leftPosition = this.chart.plotLeft,
+            topPosition = this.chart.plotTop+5,
+            space = 2;
+        this.zoomText.attr({
+            x: leftPosition,
+            y: topPosition + 15
+        });
+        leftPosition += this.zoomText.getBBox().width;
+        for (var i = 0; i < this.buttons.length; i++) {
+            this.buttons[i].attr({
+                x: leftPosition,
+                y: topPosition
+            });
+            leftPosition += this.buttons[i].width + space;
+        }
+    };
 
-        rangeSelector: {
 
-            buttons: [{
-                type: 'day',
-                count: 3,
-                text: '3d'
-            }, {
-                type: 'week',
-                count: 1,
-                text: '1w'
-            }, {
-                type: 'month',
-                count: 1,
-                text: '1m'
-            }, {
-                type: 'month',
-                count: 6,
-                text: '6m'
-            }, {
-                type: 'year',
-                count: 1,
-                text: '1y'
-            }, {
-                type: 'all',
-                text: 'All'
-            }],
-            selected: 3
-        },
+    var chartingOptions = {
 
-        yAxis: {
-            title: {
-                text: 'Temperature (°C)'
-            }
-        },
+    };
 
-        title: {
-            text: 'Hourly temperatures in Vik i Sogn, Norway, 2009-2015'
-        },
+    console.log('XXX', jugalsLib.getBasicChartOptions());
 
-        subtitle: {
-            text: 'Built chart in ...' // dummy text to reserve space for dynamic subtitle
-        },
+    chartingOptions = Object.assign({}, jugalsLib.getBasicChartOptions(), chartingOptions);
 
-        series: [{
-            name: 'Temperature',
-            data: data.values,
-            pointStart: 0,
-            pointInterval: data.dataBlockReceiveTime,
-            tooltip: {
-                valueDecimals: 1,
-                valueSuffix: '°C'
-            }
-        }]
-
-    });
+    var chart = new Highcharts.StockChart(chartingOptions);
     // var g = new Dygraph(document.getElementById("graph"),
     //   [data.values, data.values.map((item, index) => {
     //     return data.dataBlockReceiveTime * (index + 1)})], {
@@ -87,3 +54,99 @@ fetch(`http://localhost:3005/file/ch1.bin`, {
   .catch(error => {
     console.log('GET request `/book/` faild', error);
   });
+
+
+  /*
+  Highcharts.stockChart('container', {
+      chart: {
+          events: {
+              load: function () {
+                  this.setTitle(null, {
+                      text: 'Built chart in ' + (new Date() - start) + 'ms'
+                  });
+              }
+          },
+          zoomType: 'x'
+      },
+
+      rangeSelector: {
+
+        inputEnabled: false,
+        buttons: [{
+            type: 'all',
+            text: 'All'
+        }],
+          // selected: 3
+      },
+
+      plotOptions: {
+      area: {
+          fillColor: {
+              linearGradient: {
+                  x1: 0,
+                  y1: 0,
+                  x2: 0,
+                  y2: 1
+              },
+              stops: [
+                  [0, Highcharts.getOptions().colors[0]],
+                  [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+              ]
+          },
+          marker: {
+              radius: 2
+          },
+          lineWidth: 1,
+          states: {
+              hover: {
+                  lineWidth: 1
+              }
+          },
+          threshold: null
+      },
+      dataLabels: {
+          enabled: true,
+          format: '{y}'
+      }
+  },
+
+      yAxis: {
+          title: {
+              text: 'Signal'
+          },
+          min: data.min,
+          max: data.max,
+      },
+
+      xAxis: {
+        title: {
+            text: 'time'
+        },
+        // categories: xValues,
+        labels: {
+            formatter: function () {
+                return '<p>' + xValues[this.value].toFixed(5) + '</p>';
+            }
+        }
+      },
+
+      title: {
+          text: 'Signals - file: ' + data.fileName,
+      },
+
+      subtitle: {
+          text: 'Built chart in ...' // dummy text to reserve space for dynamic subtitle
+      },
+
+      series: [{
+          name: 'Signal',
+          data: data.values,
+          pointStart: data.values[0],
+          pointInterval: 1,
+          // tooltip: {
+          //     valueDecimals: 4,
+          // }
+      }]
+
+  });
+  */
